7c1a6474ee91e3b9bdec3f602da37674d67394f9
Mohammad Ashif <mohammadashif510@gmail.com>
2025-08-20 21:13:22 +0600

    register form added

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo temporibus voluptatum quisquam consectetur repellat tenetur libero quibusdam illo odio numquam fugiat velit cumque magni atque, consequatur voluptatibus voluptates assumenda. Ea quas distinctio ut obcaecati, quod velit cum suscipit culpa recusandae inventore itaque at provident ullam porro eligendi sed, tempore vero?</p>
+        </div>
+    );
+};
+
+export default About;
